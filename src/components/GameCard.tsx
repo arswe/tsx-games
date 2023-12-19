c7ff652621338d71ad4d6fdb356c3992cc072bd4
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-19 11:40:44 +0600

    refactoring

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -18,7 +18,7 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top}/></Heading>
+        <Heading fontSize='1xl'>{game.name}<Emoji rating={game.rating_top}/></Heading>
       </CardBody>
     </Card>
   )
