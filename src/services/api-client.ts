3c44b06c09fe3003fe9a9ae6b29671ce2dca6d6c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-19 12:55:02 +0600

    Reactoring

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,29 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
-  next?: string;
-  previous?: string;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: 'c16116b9ceaa4e4c907c6d94c84e87f7',
   },
 });
+
+class APIClient<T> {
+  endpint: string;
+
+  constructor(endpoint: string) {
+    this.endpint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+    .get<FetchResponse<T>>(this.endpint, config)
+    .then((res) => res.data);
+  };
+}
+
+export default APIClient;
