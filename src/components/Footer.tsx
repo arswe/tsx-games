4d01822be23d5ce0013c701270347159d6fe3f02
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-21 00:53:43 +0600

    chnage name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Text } from '@chakra-ui/react';
 const Footer = () => {
     return (
         <>
-            <Text fontSize='2xl' textAlign='center'>© 2024 GameHub</Text>
+            <Text fontSize='2xl' textAlign='center'>© 2024 Abdur Rahman</Text>
         </>
     );
 };
