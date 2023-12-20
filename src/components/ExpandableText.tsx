f7b24652b5a63620e511fab0910d67654d9a8d09
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-20 15:16:51 +0600

    Build Game Attributes

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -7,13 +7,9 @@ interface Props {
 
 const ExpandableText = ({ children }: Props) => {
   const [expanded, setExpanded] = useState(false);
-
   const limit = 200;
-
   if (!children) return null;
-
   if (children.length <= limit) return <Text>{children}</Text>;
-
   const summary = expanded ? children : children.substring(0, limit) + '....';
 
   return (
