94ff4128a00d58195767a4922014aa3706cb9852
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:16:16 +0600

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import { Text } from '@chakra-ui/react'
 
 const Footer = () => {
-  return (
-    <>
-      <Text fontSize='2xl' textAlign='center'>
-        © 2024 Abdur Rahman
-      </Text>
-    </>
-  )
+	return (
+		<>
+			<Text fontSize='2xl' textAlign='center'>
+				© 2024 Abdur Rahman
+			</Text>
+		</>
+	)
 }
 
 export default Footer
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,29 +7,29 @@ import Emoji from './Emoji'
 import PlatformIconList from './PlatformIconList'
 
 interface Props {
-  game: Game
+	game: Game
 }
 
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms?.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <Link to={'/games/' + game.slug}>
-            <Heading fontSize='1xl'>{game.name}</Heading>
-          </Link>
-          <Emoji rating={game.rating_top} />
-        </HStack>
-      </CardBody>
-    </Card>
-  )
+	return (
+		<Card>
+			<Image src={getCroppedImageUrl(game.background_image)} />
+			<CardBody>
+				<HStack justifyContent='space-between' marginBottom={3}>
+					<PlatformIconList
+						platforms={game.parent_platforms?.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
+				<HStack justifyContent='space-between' marginBottom={3}>
+					<Link to={'/games/' + game.slug}>
+						<Heading fontSize='1xl'>{game.name}</Heading>
+					</Link>
+					<Emoji rating={game.rating_top} />
+				</HStack>
+			</CardBody>
+		</Card>
+	)
 }
 
 export default GameCard
