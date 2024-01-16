bc971fb878297842259f2eb87c0e0656132e7279
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:20:12 +0600

    createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,13 @@ import theme from './themes/theme'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </ChakraProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+				<ReactQueryDevtools />
+			</QueryClientProvider>
+		</ChakraProvider>
+	</React.StrictMode>
 )
