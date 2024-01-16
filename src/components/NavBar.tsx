58eea2d8caa13895a7444936b6b424ef7b514e29
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:17:18 +0600

    Update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,15 +5,15 @@ import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 
 const NavBar = () => {
-  return (
-    <HStack padding='10px'>
-      <Link to='/'>
-        <Image src={logo} boxSize='60px' objectFit='cover' />
-      </Link>
-      <SearchInput />
-      <ColorModeSwitch />
-    </HStack>
-  )
+	return (
+		<HStack padding='10px'>
+			<Link to='/'>
+				<Image src={logo} boxSize='60px' objectFit='cover' />
+			</Link>
+			<SearchInput />
+			<ColorModeSwitch />
+		</HStack>
+	)
 }
 
 export default NavBar
