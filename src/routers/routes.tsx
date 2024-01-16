2169a1fb19c6bbb53a8597337461b6567793050c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:19:25 +0600

    router

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -5,15 +5,15 @@ import HomePage from '../pages/HomePage'
 import Layout from '../pages/Layout'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: 'games/:slug', element: <GameDetailPage /> },
-    ],
-  },
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <HomePage /> },
+			{ path: 'games/:slug', element: <GameDetailPage /> },
+		],
+	},
 ])
 
 export default router
