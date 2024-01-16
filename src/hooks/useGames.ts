755031154ce3e1cefaf5dc6f6e8df8cfbd84a392
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:18:05 +0600

    useGames

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,26 +7,26 @@ import useGameQueryStore from '../stores/store'
 const apiClient = new APIClient<Game>('/games')
 
 const useGames = () => {
-  const gameQuery = useGameQueryStore((s) => s.gameQuery)
+	const gameQuery = useGameQueryStore((s) => s.gameQuery)
 
-  return useInfiniteQuery<FetchResponse<Game>, Error>({
-    queryKey: ['games', gameQuery],
-    queryFn: ({ pageParam = 1 }) =>
-      apiClient.getAll({
-        params: {
-          genres: gameQuery.genreId,
-          parent_platforms: gameQuery.platformId,
-          ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText,
-          page: pageParam,
-        },
-      }),
-    getNextPageParam: (lastPage, allPages) => {
-      return lastPage.next ? allPages.length + 1 : undefined
-    },
-    staleTime: ms('24h'),
-    initialPageParam: 1, // Add the initialPageParam property
-  })
+	return useInfiniteQuery<FetchResponse<Game>, Error>({
+		queryKey: ['games', gameQuery],
+		queryFn: ({ pageParam = 1 }) =>
+			apiClient.getAll({
+				params: {
+					genres: gameQuery.genreId,
+					parent_platforms: gameQuery.platformId,
+					ordering: gameQuery.sortOrder,
+					search: gameQuery.searchText,
+					page: pageParam,
+				},
+			}),
+		getNextPageParam: (lastPage, allPages) => {
+			return lastPage.next ? allPages.length + 1 : undefined
+		},
+		staleTime: ms('24h'),
+		initialPageParam: 1, // Add the initialPageParam property
+	})
 }
 
 export default useGames
