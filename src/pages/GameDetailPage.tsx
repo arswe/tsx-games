bd60d8bb2fc3bf08c069e6f399f37ac0777db6fe
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:19:02 +0600

    GameDetailPage

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -7,26 +7,26 @@ import GameTrailer from '../components/GameTrailer'
 import useGame from '../hooks/useGame'
 
 const GameDetailPage = () => {
-  const { slug } = useParams()
-  const { data: game, isLoading, error } = useGame(slug!)
+	const { slug } = useParams()
+	const { data: game, isLoading, error } = useGame(slug!)
 
-  if (isLoading) return <Spinner />
+	if (isLoading) return <Spinner />
 
-  if (error || !game) throw error
+	if (error || !game) throw error
 
-  return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
-      <GridItem>
-        <Heading>{game.name}</Heading>
-        <ExpandableText>{game.description_raw}</ExpandableText>
-        <GameAttributes game={game} />
-      </GridItem>
-      <GridItem>
-        <GameTrailer gameId={game.id} />
-        <GameScreenshots gameId={game.id} />
-      </GridItem>
-    </SimpleGrid>
-  )
+	return (
+		<SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+			<GridItem>
+				<Heading>{game.name}</Heading>
+				<ExpandableText>{game.description_raw}</ExpandableText>
+				<GameAttributes game={game} />
+			</GridItem>
+			<GridItem>
+				<GameTrailer gameId={game.id} />
+				<GameScreenshots gameId={game.id} />
+			</GridItem>
+		</SimpleGrid>
+	)
 }
 
 export default GameDetailPage
