170ca94575547be81d4ab668fc5fedebffb920d7
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 22:18:15 +0600

    useGenres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -7,11 +7,11 @@ import APIClient from '../services/api-client'
 const apiClient = new APIClient<Genre>('/genres')
 
 const useGenres = () =>
-  useQuery({
-    queryKey: ['genres'],
-    queryFn: apiClient.getAll,
-    staleTime: ms('24h'),
-    initialData: genres,
-  })
+	useQuery({
+		queryKey: ['genres'],
+		queryFn: apiClient.getAll,
+		staleTime: ms('24h'),
+		initialData: genres,
+	})
 
 export default useGenres
